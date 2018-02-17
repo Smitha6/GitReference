26379ed7e2c326aade9daa03fb75b76b41747955
Smitha6 <smitha6@g.cofc.edu>
2018-02-16 21:29:58 -0500

    Initial Commit

--- app/src/main/java/com/example/alex/gitreference/MainActivity.java
+++ app/src/main/java/com/example/alex/gitreference/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.alex.gitreference;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    Command command;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView.findViewById(R.id.listView);
+
+        command = new Command();
+        command.setCommand("clone");
+        //command.setExample("git clone ssh://user@domain.com/repo.git");
+        command.setExplanation("Clone an existing repository");
+
+
+
     }
 }
