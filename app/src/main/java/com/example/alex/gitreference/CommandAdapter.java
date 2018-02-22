987594a632d8160698c0c483f8a9b5836f7f1d77
Smitha6 <smitha6@g.cofc.edu>
2018-02-22 18:03:43 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/alex/gitreference/CommandAdapter.java
@@ -0,0 +1,55 @@
+package com.example.alex.gitreference;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Alex on 2/16/2018.
+ */
+
+public class CommandAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private LayoutInflater mInflater;
+    private ArrayList<GitReference> mDataSource;
+
+    public CommandAdapter(Context context, ArrayList<GitReference> commands)
+    {
+        mContext = context;
+        mDataSource = commands;
+        mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+
+    }
+    @Override
+    public int getCount() {return mDataSource.size();}
+
+    @Override
+    public Object getItem(int i) {return mDataSource.get(i);}
+
+    @Override
+    public long getItemId(int i) {return i;}
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup)
+    {
+        View rowView = mInflater.inflate(android.R.layout.simple_expandable_list_item_2, viewGroup, false);
+
+        TextView commandView = rowView.findViewById(android.R.id.text1);
+        TextView commandExampleView = rowView.findViewById(android.R.id.text2);
+
+        GitReference command = (GitReference) getItem(position);
+        commandView.setText(command.getCommand());
+        commandExampleView.setText(command.getExample());
+
+        return rowView;
+    }
+}
+
+/
